a6e06f260b828fd7cb35d670d3c8e6f39b8d3444
cxs7700 <christophersavan@gmail.com>
2022-01-10 11:15:00 -0500

    moved to separate repo

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd39070b7-a3d3-45df-a167-783077c21868';
+export const MODAL_CLOSE_SECONDS = 2.5;
